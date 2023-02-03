2443a6db7a5870f260b25ae8b5983d4143cc9a2b
Taras Kozodoi <acateely@gmail.com>
2023-02-03 20:33:42 +0200

    add style for header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,17 @@
 import {Fragment} from "react"
 import { Link } from "react-router-dom"
 import {Outlet} from "react-router-dom"
+import "./Header.css"
 
 const Header = () => {
     return (
         <Fragment>
             <header>
-                <Link to="/" className="nav_link">Products</Link>
-                <a className="nav_link">Categories</a>
-                <a className="nav_link">Search</a>
+                <div>
+                    <Link to="/" className="nav_link">Products</Link>
+                    <a className="nav_link">Categories</a>
+                    <a className="nav_link">Search</a>
+                </div>
                 <a className="nav_link">Login</a>
             </header>
             <div className="content">
