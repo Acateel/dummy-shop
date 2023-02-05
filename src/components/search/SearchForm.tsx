2bd222fa6d6e0bd31f4227655f4c89cffbb29cd8
Taras Kozodoi <acateely@gmail.com>
2023-02-05 17:38:32 +0200

    add style into search form component

--- src/components/search/SearchForm.tsx
+++ src/components/search/SearchForm.tsx
@@ -1,5 +1,14 @@
+import "./SearchForm.css"
+
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  return (
+    <form className="search_form">
+        <input className="search" type="text" placeholder="Search.." name="search"/>
+        <button className="search_submit" type="submit">
+            <img src="search_icon.png"/>
+        </button>
+    </form>
+  )
 };
 
 export default SearchForm;
