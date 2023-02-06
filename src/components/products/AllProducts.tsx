151c2d596a0c879195064db62cdab9f04d4167bd
Taras Kozodoi <acateely@gmail.com>
2023-02-06 21:17:58 +0200

    add load more button in all products component

--- src/components/products/AllProducts.tsx
+++ src/components/products/AllProducts.tsx
@@ -1,9 +1,15 @@
 import { connect, ConnectedProps } from "react-redux";
 import ProductsList from "./ProductsList";
-import { fetchProducts } from "../../state/creators";
+import { fetchProducts, appendProductsByLimit } from "../../state/creators";
 import { Fragment, useEffect } from "react";
+import { RootState } from "../../state/store";
+import "./LoadButton.css";
 
-const connector = connect(null, { fetchProducts });
+const mapState = (state: RootState) => ({
+  products: Object.values(state.products),
+});
+
+const connector = connect(mapState, { fetchProducts, appendProductsByLimit });
 
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
@@ -11,10 +17,22 @@ const AllProducts = (props: PropsFromRedux) => {
   useEffect(() => {
     props.fetchProducts();
   }, []);
+
+  const onLoadButtonClick = () => {
+    props.appendProductsByLimit(30, props.products.length);
+  };
+
   return (
     <Fragment>
       <h1>Product List</h1>
       <ProductsList />
+      <div className="load_button_div">
+        {props.products.length > 0 && props.products.length < 100 && (
+          <button className="load_button" onClick={onLoadButtonClick}>
+            Load more...
+          </button>
+        )}
+      </div>
     </Fragment>
   );
 };
