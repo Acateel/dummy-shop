7c6dd9eb2f1985d9258367f71b332f8e2e33e6f4
Taras Kozodoi <acateely@gmail.com>
2023-02-07 21:33:52 +0200

    add style for image view component

--- src/components/product/ImageView.tsx
+++ src/components/product/ImageView.tsx
@@ -1,19 +1,28 @@
 import { useState } from "react";
+import "./ImageView.css";
 
 const ImageView = (props: { images: string[] }) => {
   const [currentImg, setCurrentImg] = useState(props.images[0]);
 
   const renderedSelectImg = () =>
     props.images.map((image) => (
-      <img key={image} className="image_view_select_image" src={image} />
+      <div
+        key={image}
+        className={`image_view_select_image ${
+          image === currentImg && "image_selected"
+        }`}
+        onMouseEnter={() => setCurrentImg(image)}
+      >
+        <img src={image} />
+      </div>
     ));
 
   return (
     <div className="image_view">
-      <div className="image_view_select">
-        {renderedSelectImg()}
+      <div className="image_view_select">{renderedSelectImg()}</div>
+      <div className="image_view_image_box">
+        <img className="image_view_image" src={currentImg} />
       </div>
-      <img className="image_view_image" src={currentImg} />
     </div>
   );
 };
