9e286f11fe24cc89fc0f54748aa3a81251469ebc
Taras Kozodoi <acateely@gmail.com>
2023-02-09 22:10:36 +0200

    add style for modal logout

--- src/components/auth/Logout.tsx
+++ src/components/auth/Logout.tsx
@@ -1,13 +1,35 @@
 import { Fragment } from "react";
+import { connect, ConnectedProps } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Modal from "../modal/Modal";
+import { logoutAuthUser } from "../../state/creators";
 
-const Logout = () => {
+const connector = connect(null, { logoutAuthUser });
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+const Logout = (props: PropsFromRedux) => {
   const navigate = useNavigate();
+
+  const onLogout = () => {
+    props.logoutAuthUser();
+    navigate("/");
+  };
 
   const onDismiss = () => {
     navigate("/");
   };
+
+  const actions = (
+    <Fragment>
+      <button onClick={onLogout} className="modal_logout_button">
+        Logout
+      </button>
+      <button onClick={onDismiss} className="modal_dismiss_button">
+        Cancel
+      </button>
+    </Fragment>
+  );
 
   return (
     <Fragment>
@@ -15,11 +37,11 @@ const Logout = () => {
       <Modal
         title="Logout?"
         content="Are you logout now?"
-        actions={<button></button>}
+        actions={actions}
         onDismiss={onDismiss}
       />
     </Fragment>
   );
 };
 
-export default Logout;
+export default connector(Logout);
--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,17 +1,23 @@
-import { ReactElement } from "react";
+import { MouseEventHandler, ReactElement } from "react";
 import { createPortal } from "react-dom";
 import "./Modal.css";
 
 type Props = {
   title: string;
   content: string;
-  actions: ReactElement;
-  onDismiss: Function;
+  actions: JSX.Element;
+  onDismiss: MouseEventHandler<HTMLDivElement>;
 };
 
 const Modal = (props: Props) => {
   return createPortal(
-    <div className="modal">Modal</div>,
+    <div onClick={props.onDismiss} className="modal">
+      <div className="modal_form" onClick={(e) => e.stopPropagation()}>
+        <h1 className="modal_header">{props.title}</h1>
+        <p className="modal_content">{props.content}</p>
+        <div className="modal_action">{props.actions}</div>
+      </div>
+    </div>,
     document.getElementById("modal") as HTMLElement
   );
 };
