76bea51b136963547b5c1e282366448315c81d11
Taras Kozodoi <acateely@gmail.com>
2023-02-09 14:57:55 +0200

    fix react router warning

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,10 +1,10 @@
 import { FormEvent, Fragment, useState } from "react";
 import { connect, ConnectedProps } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import _ from "lodash";
 import { loginAuthUser } from "../../state/creators";
 import { RootState } from "../../state/store";
 import "./Login.css";
+import { Navigate } from "react-router-dom";
 
 const mapState = (state: RootState) => ({
   auth: state.auth,
@@ -15,7 +15,6 @@ const connector = connect(mapState, { loginAuthUser });
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
 const Login = (props: PropsFromRedux) => {
-  const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
@@ -28,8 +27,7 @@ const Login = (props: PropsFromRedux) => {
     !_.isEmpty(props.auth) && !_.hasIn(props.auth, "error");
 
   if (isLogged) {
-    navigate("/");
-    return <h1>You are logged in</h1>;
+    return <Navigate to="/" replace={true} />;
   }
 
   return (
